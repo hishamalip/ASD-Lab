451115a44e9e9c05bec1b140b9c07f69c3de7d90
hishamalip <hishamalip@gmail.com>
2019-11-24 16:43:28 +0530

    subject wise results js added

--- result_analysis/views/subject.js
+++ result_analysis/views/subject.js
@@ -3,7 +3,7 @@ let { PythonShell } = require('python-shell')
 PythonShell.run('subject.py', null,function (err, results) {
   if (err) throw err;
   // results is an array consisting of messages collected during execution
-//  results.forEach(result => {console.log( result);}) 
+  //  results.forEach(result => {console.log( result);}) 
  let length=results.length;
  let ce_subject= results.slice(0,49);
  let me_subject= results.slice(51,95);
@@ -13,12 +13,12 @@ PythonShell.run('subject.py', null,function (err, results) {
  let cs_subject= results.slice(246,296);
  let ae_subject= results.slice(296,347);
 
-  ce_subject.forEach(result => {console.log(result)});
-  me_subject.forEach(result => {console.log(result)});
-  ee_subject.forEach(result => {console.log(result)});
-  ie_subject.forEach(result => {console.log(result)});
-  ec_subject.forEach(result => {console.log(result)});
-  cs_subject.forEach(result => {console.log(result)});
-  ae_subject.forEach(result => {console.log(result)});
+  // ce_subject.forEach(result => {console.log(result)});
+  // me_subject.forEach(result => {console.log(result)});
+  // ee_subject.forEach(result => {console.log(result)});
+  // ie_subject.forEach(result => {console.log(result)});
+  // ec_subject.forEach(result => {console.log(result)});
+  // cs_subject.forEach(result => {console.log(result)});
+  // ae_subject.forEach(result => {console.log(result)});
 });
 
