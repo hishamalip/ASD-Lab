9416a8eb0cbed873bb104441459d49357af4cb02
hishamalip <hishamalip@gmail.com>
2019-11-27 18:25:37 +0530

    added function to fing total result

--- result_analysis/routes/result.js
+++ result_analysis/routes/result.js
@@ -10,15 +10,16 @@ module.exports = {
             // results is an array consisting of messages collected during execution
             //  results.forEach(result => {console.log( result);}) 
             let length = results.length;
-            let ce_result = results.slice(1, 5).join('\n');;
+            let ce_result = results.slice(1, 5).join('\n');
             // ce_result= ce_result(); 
             // ce_result1 = ce_result.forEach() 
             let ee_result = results.slice(7, 11).join('\n');
-            let me_result = results.slice(13, 17).join('\n');;
-            let ie_result = results.slice(19, 23).join('\n');;
-            let ec_result = results.slice(25, 29).join('\n');;
-            let cs_result = results.slice(31, 35).join('\n');;
-            let ae_result = results.slice(37, 41).join('\n');;
+            let me_result = results.slice(13, 17).join('\n');
+            let ie_result = results.slice(19, 23).join('\n');
+            let ec_result = results.slice(25, 29).join('\n');
+            let cs_result = results.slice(31, 35).join('\n');
+            let ae_result = results.slice(37, 41).join('\n');
+            let tt_result = results.slice(42, 47).join('\n')
           
             // ce_result.forEach(result => { console.log(result) });
             // me_result.forEach(result => { console.log(result) });
@@ -29,7 +30,7 @@ module.exports = {
             // ae_result.forEach(result => { console.log(result) });
             // console.log(ce_result);
             res.render('result.ejs', {
-                title: 'KTU Result Analysis | Test'
+                title: 'KTU Result Analysis'
                 ,message: '',
                 ce_result: ce_result,
                 ee_result : ee_result,
@@ -37,7 +38,8 @@ module.exports = {
                 ie_result : ie_result,
                 ec_result : ec_result,
                 cs_result : cs_result,
-                ae_result : ae_result
+                ae_result : ae_result,
+                tt_result : tt_result
             });
         });
     },
---
+++ result_analysis/routes/total_result.js
@@ -0,0 +1,28 @@
+let { PythonShell } = require('python-shell');
+// let ce_result;
+PythonShell.run(__dirname+'/total_result.py', null, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  //  results.forEach(result => {console.log( result);}) 
+  let length = results.length;
+  let ce_result = results.slice(0, 5);
+  let me_result = results.slice(6, 11);
+  let ee_result = results.slice(12, 17);
+  let ie_result = results.slice(18, 23);
+  let ec_result = results.slice(24, 29);
+  let cs_result = results.slice(30, 35);
+  let ae_result = results.slice(36, 41);
+  let te_result = results.slice(42, 47);
+  // ce_result.forEach(result => { console.log(result) });
+  // me_result.forEach(result => { console.log(result) });
+  // ee_result.forEach(result => { console.log(result) });
+  // ie_result.forEach(result => { console.log(result) });
+  // ec_result.forEach(result => { console.log(result) });
+  // cs_result.forEach(result => { console.log(result) });
+  // ae_result.forEach(result => { console.log(result) });
+
+  module.exports = {
+    ce_result:ce_result
+  }
+});
+
